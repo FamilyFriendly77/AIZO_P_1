c860ad6a583995e0a1d05fa35951595d27ae14e8
Krystian <krystian@mac.home>
2026-03-28 23:04:27 +0100

    minor fixes

--- config.cpp
+++ config.cpp
@@ -1,11 +1,16 @@
 #include "config.hpp"
 Setting<unsigned int> Config::randomSeed = Setting<unsigned int>("randomSeed");
+
 Setting<bool> Config::inTestMode = Setting<bool>("TestMode", false);
+
 Setting<std::string> Config::testFilename =
     Setting<std::string>("testFileName");
+
 Setting<int> Config::testArrayLen = Setting<int>("testArrayLen");
+
 Setting<std::string> Config::configFile =
     Setting<std::string>("configFilename");
+
 void Config::setConfigFile(std::string filename) {
   configFile.setSetting(filename);
 }
