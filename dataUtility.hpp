a32b1f382e7deec61a1a5cdc5d164cae1c23cb6e
Krystian <krystian@mac.home>
2026-03-28 23:03:02 +0100

    reading array from file, To be tested, next to do: write unit tests and testing toolsw

--- dataUtility.hpp
+++ dataUtility.hpp
@@ -1,11 +1,47 @@
 #include "config.hpp"
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+#define RAND_MAX INT_MAX
+
 template <typename T> class DataUtility {
 private:
   int arrayLen;
 
 public:
-  DataUtility<T>();
-  void setArrayLen(int len);
-  T *generateRandomArray();
-  T *readArrayFromFile();
+  DataUtility<T>() {
+    arrayLen = Config::testArrayLen.getValue();
+    unsigned int randomSeed = Config::randomSeed.getValue();
+  };
+  void setArrayLen(int len) { arrayLen = len; };
+  T *generateRandomArray() {
+    T *array;
+    array = (T *)malloc(arrayLen * (sizeof(T)));
+    for (int i = 0; i < arrayLen; i++) {
+      array[i] = rand();
+    };
+
+    return array;
+    // TBT
+  };
+
+  T *readArrayFromFile() {
+    T *array;
+    std::ifstream testFile(Config::testFilename.getValue());
+    std::string buff;
+    // checking if the first line containing array len exists, if not array
+    // can't be read
+    if (!std::getline(testFile, buff)) {
+      return NULL;
+    }
+    std::cout << buff;
+    int length = std::stoi(buff);
+    array = (T *)malloc(length * (sizeof(T)));
+    for (int i = 0; i < length; i++) {
+      std::getline(testFile, buff);
+      array[i] = std::stoi(buff);
+    }
+    // TBF && TBT
+  };
 };
